ab624e88d7c22aa84a60f07c8d208e9f963cd653
Emil Hernvall <ehernva@amazon.com>
2022-05-16 10:55:59 +0200

    feat: Add numeric ops

--- src/bin/csvquery.rs
+++ src/bin/csvquery.rs
@@ -1,9 +1,7 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
 use std::error::Error;
 
-use serde::{Serialize, Deserialize};
+use serde_json::Value;
 
 use lalrpop_util::lalrpop_mod;
 
@@ -15,43 +13,11 @@ pub enum StringTransform {
 }
 
 impl StringTransform {
-    pub fn evaluate(&self, input: Value) -> Value {
+    pub fn evaluate(&self, input: Value) -> Option<Value> {
+        let s = input.as_str()?;
         match self {
-            StringTransform::UpperCase => input.map_string(|x| x.to_uppercase()),
-            StringTransform::LowerCase => input.map_string(|x| x.to_lowercase()),
-        }
-    }
-}
-
-#[derive(Debug,Clone)]
-pub enum Value {
-    Literal(String),
-    Number(f64),
-}
-
-impl PartialEq for Value {
-    fn eq(&self, rhs: &Value) -> bool {
-        match (self, rhs) {
-            (Value::Literal(a), Value::Literal(b)) if a == b => true,
-            (Value::Number(a), Value::Number(b)) if a == b => true,
-            (Value::Literal(a), Value::Number(b)) |
-            (Value::Number(b), Value::Literal(a)) => {
-                let a: f64 = match a.parse() {
-                    Ok(x) => x,
-                    Err(_e) => return false,
-                };
-                a == *b
-            },
-            _ => false,
-        }
-    }
-}
-
-impl Value {
-    fn map_string(self, f: impl Fn(String) -> String) -> Value {
-        match self {
-            Value::Literal(s) => Value::Literal(f(s)),
-            _ => self,
+            StringTransform::UpperCase => Some(Value::String(s.to_uppercase())),
+            StringTransform::LowerCase => Some(Value::String(s.to_lowercase())),
         }
     }
 }
@@ -65,11 +31,11 @@ pub enum ValueExpr {
 impl ValueExpr {
     pub fn evaluate(&self, row: &HashMap<String, Value>) -> Option<Value> {
         match self {
-            ValueExpr::Field(field) => row.get(field).map(|x| x.clone()),
+            ValueExpr::Field(field) => row.get(field).cloned(),
             ValueExpr::Value(value) => Some(value.clone()),
-            ValueExpr::Transform(transform, ref expr) => Some(
-                transform.evaluate(expr.evaluate(row)?),
-            ),
+            ValueExpr::Transform(transform, ref expr) => {
+                transform.evaluate(expr.evaluate(row)?)
+            },
         }
     }
 }
@@ -77,48 +43,46 @@ impl ValueExpr {
 pub enum BoolExpr {
     Eq(ValueExpr, ValueExpr),
     Neq(ValueExpr, ValueExpr),
+    Lt(ValueExpr, ValueExpr),
+    Gt(ValueExpr, ValueExpr),
     Not(Box<BoolExpr>),
     And(Box<BoolExpr>, Box<BoolExpr>),
+    Or(Box<BoolExpr>, Box<BoolExpr>),
 }
 
 impl BoolExpr {
-    pub fn evaluate(&self, row: &HashMap<String, Value>) -> bool {
+    pub fn evaluate(&self, row: &HashMap<String, Value>) -> Option<bool> {
         match self {
-            BoolExpr::Eq(lh, rh) => rh.evaluate(row) == lh.evaluate(row),
-            BoolExpr::Neq(lh, rh) => rh.evaluate(row) != lh.evaluate(row),
-            BoolExpr::Not(expr) => !expr.evaluate(row),
-            BoolExpr::And(lh, rh) => lh.evaluate(row) && rh.evaluate(row),
+            BoolExpr::Eq(lh, rh) => Some(rh.evaluate(row)? == lh.evaluate(row)?),
+            BoolExpr::Neq(lh, rh) => Some(rh.evaluate(row)? != lh.evaluate(row)?),
+            BoolExpr::Not(expr) => Some(!expr.evaluate(row)?),
+            BoolExpr::And(lh, rh) => Some(lh.evaluate(row)? && rh.evaluate(row)?),
+            BoolExpr::Or(lh, rh) => Some(lh.evaluate(row)? || rh.evaluate(row)?),
+            BoolExpr::Lt(lh, rh) => {
+                let fst = lh.evaluate(row)?.as_i64()?;
+                let snd = rh.evaluate(row)?.as_i64()?;
+                Some(fst < snd)
+            },
+            BoolExpr::Gt(lh, rh) => {
+                let fst = lh.evaluate(row)?.as_i64()?;
+                let snd = rh.evaluate(row)?.as_i64()?;
+                Some(fst > snd)
+            },
         }
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let filename = std::env::args().nth(1).unwrap();
-
-    let query = std::env::args().nth(2).unwrap();
+    let query = std::env::args().nth(1).unwrap();
     let parser = dsl::BoolExprParser::new();
     let query: BoolExpr = parser.parse(&query).unwrap();
 
-    let file = File::open(filename)?;
-    let file = BufReader::new(file);
+    let data = std::fs::read_to_string("whales.json")?;
+    let whales: Vec<HashMap<String, Value>> = serde_json::from_str(&data)?;
 
-    let mut lines = file.lines().filter_map(|x| x.ok());
-    let header = lines.next().unwrap();
-    let header = header.split(",").collect::<Vec<_>>();
-
-    for line in lines {
-        let fields = line.split(",").collect::<Vec<_>>();
-        if fields.len() != header.len() {
-            continue;
-        }
-
-        let row = header.iter()
-            .zip(fields.iter())
-            .map(|(k, v)| (k.to_string(), Value::Literal(v.to_string())))
-            .collect::<HashMap<_, _>>();
-
-        if query.evaluate(&row) {
-            dbg!(row);
+    for whale in whales {
+        if matches!(query.evaluate(&whale), Some(true)) {
+            println!("{}", serde_json::to_string(&whale)?);
         }
     }
 
